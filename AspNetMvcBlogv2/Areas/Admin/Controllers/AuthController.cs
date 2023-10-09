0d063c671bb6049c2db1e39315bca7be2a9b803a
Wtell64 <75840262+Wtell64@users.noreply.github.com>
2023-10-09 14:04:41 +0300

    Admin panel and Admin Auth pages

---
+++ AspNetMvcBlogv2/Areas/Admin/Controllers/AuthController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetMvcBlogv2.Areas.Admin.Controllers
+{
+  [Area("Admin")]
+  public class AuthController : Controller
+  {
+    public IActionResult Login()
+    {
+      return View();
+    }
+
+    public IActionResult Register()
+    {
+      return View();
+    }
+  }
+}

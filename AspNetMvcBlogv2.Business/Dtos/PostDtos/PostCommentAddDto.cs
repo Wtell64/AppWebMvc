284fb1c944dc7a598e9e238bcd8440d0cc4a68d4
Wtell64 <75840262+Wtell64@users.noreply.github.com>
2023-10-17 23:16:36 +0300

    CRUD admin islemleri bitti

---
+++ AspNetMvcBlogv2.Business/Dtos/PostDtos/PostCommentAddDto.cs
@@ -0,0 +1,23 @@
+﻿using AspNetMvcBlogv2.Persistence.Entity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspNetMvcBlogv2.Business.Dtos.PostDtos
+{
+  public class PostCommentAddDto
+  {
+
+    public Guid Id { get; set; }
+
+    public string Content { get; set; }
+
+    public Guid PostId { get; set; }
+
+    public Guid UserId { get; set; }
+  }
+}
---
+++ AspNetMvcBlogv2.Business/Dtos/PostDtos/PostImageAddDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspNetMvcBlogv2.Business.Dtos.PostDtos
+{
+  public class PostImageAddDto
+  {
+    public Guid Id { get; set; }
+
+    public Guid PostId { get; set; }
+
+    public string ImageUrl { get; set; }
+  }
+}
